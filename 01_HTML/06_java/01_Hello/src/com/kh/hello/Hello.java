371fad65fc8b6d6071ea4733ec2d545f4061abc1
SEO JIEUN <jieun9099@gmail.com>
2025-06-17 21:39:14 +0900

    Java_6/17_2

---
+++ 01_HTML/06_java/01_Hello/src/com/kh/hello/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * * 자바의 특징
+ * 1. 객체지향 프로그래밍(Object-Oriented Progrmming, OOP) 언어
+ * - 모든 기능을 "객체"라는 단위로 구성
+ * 
+ * */
+
+
+package com.kh.hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, World!");
+
+	}
+
+}
