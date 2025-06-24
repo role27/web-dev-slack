0571f77575b092abcbff17fa5c2279aef37f65d2
SEO JIEUN <jieun9099@gmail.com>
2025-06-24 20:50:24 +0900

    12345

--- 01_HTML/06_Java/06_Object/src/com/kh/practice2/controller/SnackController.java
+++ 01_HTML/06_Java/06_Object/src/com/kh/practice2/controller/SnackController.java
@@ -1,40 +1,33 @@
 package com.kh.practice2.controller;
 
-import java.util.Arrays;
-
 import com.kh.practice2.model.Snack;
 
 public class SnackController {
 
-	private Snack sModel  = new Snack();
+	private Snack s = new Snack();
+
 	
 	
 	
-	
-	public String sKind() {
-		sModel.setKind(sModel.getKind());
-	}
-	
-	public String sName() {
-		sModel.setName(sModel.getName());
-	}
-	
-	public String sFlavor() {
-		sModel.setFlavor(sModel.getFlavor());
-	}
-	
-	public int snumOf() {
-		sModel.setNumOf(sModel.getNumOf());
-	}
-	
-	public int sPrice() {
-		sModel.setPrice(sModel.getPrice());
+	public String saveData(String kind, String name, String flavor, int numOf, int price) {
+		
+		s.setKind(kind);
+		s.setName(name);
+		s.setFlavor(flavor);
+		s.setNumOf(numOf);
+		s.setPrice(price);
+		return "저장이 완료되었습니다.";
+		
 	}
 	
 	
-	public String sResult() {
-		 return "종류 : " + sModel.getKind()
-				
+	
+	
+		
+	public String confirmData() {
+        return s.toString();
+	    }
+		
 	}
 	
 	
@@ -46,5 +39,4 @@ public class SnackController {
 	
 	
 	
-	
-}
+
