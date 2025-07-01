ea696cafe682d029578b51e40f226e2fbaa69efc
SEO JIEUN <jieun9099@gmail.com>
2025-07-02 06:27:26 +0900

    7/1_2

--- 01_HTML/06_java/13_IO/src/com/kh/object/Person.java
+++ 01_HTML/06_java/13_IO/src/com/kh/object/Person.java
@@ -6,11 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// 객체를 직렬화로 통과하려면 반드시 implements Serializable
-@NoArgsConstructor @AllArgsConstructor @Data
-public class Person implements Serializable {
 
+// 객체를 직렬화로 통과하려면 반드시 implements Serializable
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+
+public class Person implements Serializable{
 	private String name;
 	private String id;
 	
+
 }
